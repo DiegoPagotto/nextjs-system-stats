98cdc14ba5223c2867057a0667aa723d42dddefc
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-08-15 21:24:39 -0300

    feat: adds loading effect while waiting for data

--- src/app/components/SystemInfoCard/system-info-card.tsx
+++ src/app/components/SystemInfoCard/system-info-card.tsx
@@ -5,6 +5,7 @@ import SystemComponent from '../SystemComponent/system-component';
 import CPUComponent from '../CPU/cpu-body';
 import { SystemInfo } from '@/app/types/system-info';
 import MemoryComponent from '../Memory/memory-body';
+import Loading from '../Loading/loading';
 
 const SystemInfoCard = () => {
     const [systemInfo, setSystemInfo] = useState<SystemInfo | null>(null);
@@ -20,14 +21,16 @@ const SystemInfoCard = () => {
     }, []);
 
     return (
-        <div className="w-full max-w-2xl rounded overflow-hidden shadow-lg bg-slate-900">
-            <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2 text-center">
-                    System Stats
-                </div>
-            </div>
-            {systemInfo && (
+        <div className="w-full max-w-4xl md:max-w-3xl sm:max-w-2xl sm:w-11/12 rounded-xl overflow-hidden shadow-lg bg-slate-900 mx-auto">
+            {!systemInfo ? (
+                <Loading />
+            ) : (
                 <>
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2 text-center">
+                            System Stats
+                        </div>
+                    </div>
                     <SystemComponent
                         title="CPU"
                         body={<CPUComponent cpuInfo={systemInfo.cpu} />}
