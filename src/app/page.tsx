c66579541417879f96c94c60378a5443c3d05e15
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-08-15 21:23:53 -0300

    feat: lowers main container padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import SystemInfoCard from './components/SystemInfoCard/system-info-card';
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-950">
+        <main className="flex min-h-screen flex-col items-center justify-between p-12 bg-slate-950">
             <SystemInfoCard />
         </main>
     );
