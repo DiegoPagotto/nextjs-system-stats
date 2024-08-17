69ad10b6c6dd2be5800e0c24c7c81342ac7e7d10
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-08-17 13:06:13 -0300

    refact: extracts methods to get cpu and memory info

--- pages/api/system-info.ts
+++ pages/api/system-info.ts
@@ -1,31 +1,46 @@
+import { cpu } from '@/app/types/cpu';
 import { memory } from '@/app/types/memory';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import si from 'systeminformation';
+
+const getCpuInfo = async () => {
+    const cpu = await si.cpu();
+
+    return {
+        manufacturer: cpu.manufacturer,
+        brand: cpu.brand,
+        speed: cpu.speed,
+        cores: cpu.cores,
+        physicalCores: cpu.physicalCores,
+        processors: cpu.processors,
+    } as cpu;
+};
+
+const getMemoryInfo = async () => {
+    const mem = await si.mem();
+    const memLayout = await si.memLayout();
+    const memoryModules = memLayout.length;
+    const memoryModuleInfo = memoryModules > 0 ? memLayout[0] : null;
+    return {
+        total: mem.total,
+        available: mem.available,
+        used: mem.used,
+        memoryModules: memoryModules,
+        type: memoryModuleInfo?.type || '',
+        voltageConfigured: memoryModuleInfo?.voltageConfigured || 0,
+    } as memory;
+};
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
     try {
-        const cpu = await si.cpu();
-
-        const mem = await si.mem();
-        const memLayout = await si.memLayout();
-        const memoryModules = memLayout.length
-        const memoryModuleInfo = memoryModules > 0 ? memLayout[0] : null;
-        const memory: memory = {
-            total: mem.total,
-            available: mem.available,
-            used: mem.used,
-            memoryModules: memoryModules,
-            type: memoryModuleInfo?.type || '',
-            voltageConfigured: memoryModuleInfo?.voltageConfigured || 0,
-        } as memory;
+        const cpu = await getCpuInfo();
+        const memory = await getMemoryInfo();
 
         const gpu = await si.graphics();
-
         const currentLoad = await si.currentLoad();
-        
         const temp = await si.cpuTemperature();
 
         res.status(200).json({
