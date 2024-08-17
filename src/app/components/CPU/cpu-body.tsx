5c9e14487e42dbff1395c2eea4632e3411a324c5
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-08-17 13:52:10 -0300

    feat: adds cpu cores progressbar

--- src/app/components/CPU/cpu-body.tsx
+++ src/app/components/CPU/cpu-body.tsx
@@ -1,8 +1,9 @@
-import { cpu } from "@/app/types/cpu";
+import { cpu } from '@/app/types/cpu';
+import ProgressBar from '../ProgressBar/progress-bar';
 
 interface CPUComponentProps {
     cpuInfo: cpu;
-}  
+}
 
 const CPUComponent = ({ cpuInfo }: CPUComponentProps) => {
     return (
@@ -17,12 +18,25 @@ const CPUComponent = ({ cpuInfo }: CPUComponentProps) => {
                 <p>
                     <b>Speed</b>: {cpuInfo.speed} GHz
                 </p>
-                <p>
-                    <b>Cores</b>: {cpuInfo.physicalCores}
-                </p>
-                <p>
-                    <b>Threads</b>: {cpuInfo.cores.length}
-                </p>
+                <div className="flex gap-4">
+                    <p>
+                        <b>Cores</b>: {cpuInfo.physicalCores}
+                    </p>
+                    <p>
+                        <b>Threads</b>: {cpuInfo.cores.length}
+                    </p>
+                </div>
+
+                <div className="grid grid-cols-2 gap-4 text-center mt-5">
+                    {cpuInfo.cores.map((core, index) => (
+                        <div key={index}>
+                            <p>
+                                <b>Core {index + 1}</b>
+                            </p>
+                            <ProgressBar usedPercentage={core.load} />
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
