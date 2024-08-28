684343bbf4c0162243fdddcc741785d113bb8e2f
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-08-27 22:44:57 -0300

    feat: adds toaster wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+'use client';
+
+import { Toaster } from 'react-hot-toast';
 import SystemInfoCard from './components/SystemInfoCard/system-info-card';
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-12 bg-slate-950">
             <SystemInfoCard />
+
+            <Toaster />
         </main>
     );
 }
