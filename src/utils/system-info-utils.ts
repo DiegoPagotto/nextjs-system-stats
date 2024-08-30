79634636687c034a6d83a796543f35b4dbcf03ac
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-08-30 19:14:06 -0300

    feat:  creates disk info reading

--- src/utils/system-info-utils.ts
+++ src/utils/system-info-utils.ts
@@ -9,12 +9,14 @@ export const getSystemInfo = async () => {
     const gpu = await si.graphics();
     await getCurrentLoad(cpu);
     const temp = await si.cpuTemperature();
+    const disk = await getDiskInfo();
 
     return {
         cpu,
         gpu,
         memory,
         temp,
+        disk,
     };
 };
 
@@ -49,4 +51,19 @@ const getCurrentLoad = async (cpuInfo: cpu) => {
     const currentLoad = await si.currentLoad();
     cpuInfo.cores = currentLoad.cpus;
     return currentLoad;
+};
+
+const getDiskInfo = async () => {
+    const diskLayout = await si.diskLayout();
+    const fsSize = await si.fsSize();
+    return diskLayout.map((disk, index) => ({
+        device: disk.device,
+        type: disk.type,
+        name: disk.name,
+        vendor: disk.vendor,
+        size: disk.size,
+        used: fsSize[index]?.used || 0,
+        available: fsSize[index]?.size - fsSize[index]?.used || 0,
+        mount: fsSize[index]?.mount || '',
+    })) as disk[];
 };
