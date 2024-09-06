15d4b283ac3ca8256ce01c8b2ea6647045e428f4
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-09-06 20:03:12 -0300

    feat: aligns emoji and title

--- src/app/components/SystemComponent/system-component.tsx
+++ src/app/components/SystemComponent/system-component.tsx
@@ -15,9 +15,9 @@ const SystemComponent = ({
 }: SystemComponentProps) => {
     return (
         <div className="px-6 py-4">
-            <div>
-                {emoji}
-                <div className="font-bold text-xl mb-2">{title}</div>
+            <div className="font-bold text-xl mb-2">
+                {emoji && <span className="mr-3">{emoji}</span>}
+                {title}
             </div>
             <p className="text-gray-500 text-base">{children || description}</p>
         </div>
