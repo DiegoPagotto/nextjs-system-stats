cd65a3b5a6850f833063be2637faa67271e2c6a8
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-09-05 22:18:33 -0300

    feat: adds emoji to system-component

--- src/app/components/SystemComponent/system-component.tsx
+++ src/app/components/SystemComponent/system-component.tsx
@@ -4,16 +4,21 @@ interface SystemComponentProps {
     title: string;
     description?: string;
     children?: React.ReactNode;
+    emoji?: React.ReactNode;
 }
 
 const SystemComponent = ({
     title,
     description,
     children,
+    emoji,
 }: SystemComponentProps) => {
     return (
         <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">{title}</div>
+            <div>
+                {emoji}
+                <div className="font-bold text-xl mb-2">{title}</div>
+            </div>
             <p className="text-gray-500 text-base">{children || description}</p>
         </div>
     );
