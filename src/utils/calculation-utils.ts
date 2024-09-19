fa6a5a027d396e3c6a0aab3e2e559071024e49cc
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-09-18 22:50:07 -0300

    feat: creates function to format time as string

--- src/utils/calculation-utils.ts
+++ src/utils/calculation-utils.ts
@@ -8,3 +8,12 @@ export const calculateUsedPercentage = (
 ): number => {
     return Math.round((used / total) * 100);
 };
+
+export const getFormattedUptime = (uptime: number) => {
+    const days = Math.floor(uptime / (3600 * 24));
+    const hours = Math.floor((uptime % (3600 * 24)) / 3600);
+    const minutes = Math.floor((uptime % 3600) / 60);
+    const seconds = Math.floor(uptime % 60);
+
+    return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+};
