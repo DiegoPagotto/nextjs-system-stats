5ad745148e74ccd2858f528f4ba8f639b2350e00
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-10-02 10:57:35 -0300

    feat: adds footer to app

---
+++ src/app/components/Footer/footer.tsx
@@ -0,0 +1,44 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="rounded-lg shadow m-4 bg-slate-900">
+                <div className="text-gray-400 w-full mx-auto max-w-screen-xl p-4 flex justify-evenly gap-5">
+                    <span className="text-sm">
+                        <a
+                            href="https://diegopagotto.me/"
+                            className="hover:underline"
+                        >
+                            Made with 💙 by Diego Pagotto
+                        </a>
+                    </span>
+                    <a
+                        href="https://github.com/DiegoPagotto"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="hover:underline"
+                        title="GitHub"
+                    >
+                        <FontAwesomeIcon icon={faGithub} className="text-lg" />
+                    </a>
+                    <a
+                        href="https://linkedin.com/in/diego-pagotto/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="hover:underline"
+                        title="LinkedIn"
+                    >
+                        <FontAwesomeIcon
+                            icon={faLinkedin}
+                            className="text-lg"
+                        />
+                    </a>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@
 
 import { Toaster } from 'react-hot-toast';
 import SystemInfoCard from './components/SystemInfoCard/system-info-card';
+import Footer from './components/Footer/footer';
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-12 bg-slate-950">
             <SystemInfoCard />
-
+            <Footer />
             <Toaster
                 position="top-right"
                 toastOptions={{
